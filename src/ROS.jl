081b09f44339b1a434173109566852c1e13d642d
George Stavrinos <stavrinosgeo@gmail.com>
2019-01-19 19:57:45 +0200

    Modular ROS.jl

--- src/ROS.jl
+++ src/ROS.jl
@@ -6,7 +6,9 @@ using Libdl
 
 export @rosinclude, init, ok, spin, spinOnce, NodeHandle, advertise, subscribe
 
-#include("todo.jl")
+include("publisher.jl")
+include("subscriber.jl")
+include("nodehandle.jl")
 
 # -------------------------------------------------------------------
 # roscpp functions
@@ -18,27 +20,9 @@ init(node_name::String) = @cxx ros::init(Int32(length(ARGS)+1), pointer(pointer.
 
 
 # TODO support init options
-# TODO support TransportHints in subscribe function
 
 # TODO investigate this further
 NodeHandle() = @cxxnew ros::NodeHandle()
-# TODO investigate this further
-advertise(nodehandle, topic_name::String, topic_type, queue_size::Int) = icxx"$(nodehandle)->advertise<$(topic_type)>($(pointer(topic_name)), $(queue_size));"
-# TODO investigate this further
-publish(publisher, msg) = icxx"$(publisher).publish(*$(msg));"
-# TODO investigate this further
-function subscribe(nodehandle, topic_name::String, queue_size::Int, topic_type, callback)
-    #icxx"$(nodehandle)->subscribe($(pointer(topic_name)),$(queue_size),&($(test)));"
-    icxx"""
-        //std::function<void (sensor_msgs::Imu::ConstPtr)> cb = [&](sensor_msgs::Imu::ConstPtr msg) {
-        boost::function<void ($(topic_type))> cpp_callback = [&]($(topic_type) msg) {
-            $:(callback(icxx"return msg;"));
-        };
-        return $(nodehandle)->subscribe<$(topic_type)>($(pointer(topic_name)), $(queue_size), cpp_callback);
-    """
-end
-
-
 
 # -------------------------------------------------------------------
 # macro definitions
