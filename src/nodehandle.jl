0118d6cdad20ef7f145dd90b4dc9321e67b728d2
George Stavrinos <stavrinosgeo@gmail.com>
2019-01-21 20:56:36 +0200

    Added advertiseService function

--- src/nodehandle.jl
+++ src/nodehandle.jl
@@ -1,5 +1,5 @@
 #!/usr/bin/env julia
-export advertise, subscribe
+export advertise, advertiseService, subscribe, shutdown
 
 # -------------------------------------------------------------------
 # roscpp functions
@@ -9,15 +9,27 @@ export advertise, subscribe
 
 # TODO investigate this further
 advertise(nodehandle, topic_name::String, topic_type, queue_size::Int) = icxx"$(nodehandle)->advertise<$(topic_type)>($(pointer(topic_name)), $(queue_size));"
+
+function advertiseService(nodehandle, topic_name::String, topic_type, callback)
+    icxx"""
+        boost::function<bool ($(service_types[topic_type][1])&, $(service_types[topic_type][2])&)> cpp_callback = [&]($(service_types[topic_type][1]) &req, $(service_types[topic_type][2]) &res) {
+            $:(callback(icxx"return req;", icxx"return res;"));
+            return true;
+        };
+        return $(nodehandle)->advertiseService($(pointer(topic_name)), cpp_callback);
+    """
+end
+
 # TODO investigate this further
 function subscribe(nodehandle, topic_name::String, queue_size::Int, topic_type, callback)
-    #icxx"$(nodehandle)->subscribe($(pointer(topic_name)),$(queue_size),&($(test)));"
     icxx"""
-        //std::function<void (sensor_msgs::Imu::ConstPtr)> cb = [&](sensor_msgs::Imu::ConstPtr msg) {
         boost::function<void ($(topic_type))> cpp_callback = [&]($(topic_type) msg) {
             $:(callback(icxx"return msg;"));
         };
         return $(nodehandle)->subscribe<$(topic_type)>($(pointer(topic_name)), $(queue_size), cpp_callback);
     """
 end
+
+# NodeHandle, Publisher, Subscriber, ServiceServer
+shutdown(n) = @cxx n->shutdown()
 
